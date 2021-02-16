1f8d73ff22287ca74285ddadb14951d21b0a1ebc
womfeld <womfeld@hawk.iit.edu>
2021-02-15 22:48:42 -0600

    Twitter app

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,33 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by School on 2/15/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
